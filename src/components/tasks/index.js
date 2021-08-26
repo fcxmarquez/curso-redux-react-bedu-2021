0c2b39e56e8ded06a7e525de73ec9fbb25749268
Francisco <francisco.marquez.solt@gmail.com>
2021-08-26 13:52:43 -0500

    Mapear objetos

--- src/components/tasks/index.js
+++ src/components/tasks/index.js
@@ -1,15 +1,50 @@
 import React from "react";
 import { connect } from "react-redux";
 import * as tasksActions from "../../actions/tasksActions";
-
+import Spinner from "../general/Spinner";
+import Fatal from "../general/Fatal";
 class Tasks extends React.Component {
   componentDidMount() {
-    this.props.getAll()
+    this.props.getAll();
   }
+
+  showInfo = () => {
+    const { tasks, loading, error } = this.props;
+
+    if (loading) {
+      return <Spinner />;
+    }
+
+    if (error) {
+      return <Fatal msg={error} />;
+    }
+
+    return Object.keys(tasks).map((item) => (
+      <div key={item}>
+        <h2>Usuario {item}</h2>
+        <div className="tasks-container">{this.putTasks(item)}</div>
+      </div>
+    ));
+  };
+
+  putTasks = (userId) => {
+    const { tasks } = this.props;
+    const forUser = { ...tasks[userId] };
+
+    return Object.keys(forUser).map((item) => (
+      <div key={item}>
+        <input
+          type="checkbox"
+          defaultChecked={forUser[item].completed}
+        />
+        {forUser[item].title}
+      </div>
+    ));
+  };
 
   render() {
     console.log(this.props);
-    return <div>Tareas</div>;
+    return <div>{this.showInfo()}</div>;
   }
 }
 
