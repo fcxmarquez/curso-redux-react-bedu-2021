4f9f9830ff824a0eaed63040b521235adde592c2
Francisco <francisco.marquez.solt@gmail.com>
2021-08-28 22:10:19 -0500

    Error Fixed

--- src/actions/types/tasksTypes.js
+++ src/actions/types/tasksTypes.js
@@ -1,6 +1,6 @@
 export const getTasks = "GET_TASKS";
-export const loading = "LOADING_USERS";
-export const error = "ERROR_USERS";
+export const loading = "LOADING_TASKS";
+export const error = "ERROR_LOADING_TASKS";
 export const setChangeUserId = "SET_CHANGE_USER_ID"
 export const setChangeTitle = "SET_CHANGE_TITLE"
 export const setNewTask = "SET_NEW_TASK"
